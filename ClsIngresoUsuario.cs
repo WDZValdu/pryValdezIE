0b2579a2a7c4b7b0c79f8778c993115469629452
Laboratorios de Informática IES 21 <laboratoriosies@hotmail.com>
2023-10-10 14:31:19 -0300

    10102023 base de datos

--- ClsIngresoUsuario.cs
+++ ClsIngresoUsuario.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+
 
 namespace pryValdezIE
 {
     internal class ClsIngresoUsuario
     {
+        OleDbConnection ConexionBD;
+
+        public void AbrirBD()
+        {
+            try
+            {
+                ConexionBD = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0;" + "Data Source = E:\\Escritorio\\Bases de datos\\Ingreso_Usuarios.accdb");
+                ConexionBD.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);               
+            }
+        }
+        
     }
 }
